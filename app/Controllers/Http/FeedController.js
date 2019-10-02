00e4e5ccabc8002fea77dea0d07d154757a6648f
Mauricio Souza <mauricio.souza@egestora.com.br>
2019-10-02 17:53:35 -0300

    Adicionado teste do pacote Chance para gerar dados fictícios no endpoint [Post]/feed

--- app/Controllers/Http/FeedController.js
+++ app/Controllers/Http/FeedController.js
@@ -1,5 +1,6 @@
 'use strict'
 const Feed = use('App/Models/Feed')
+var Chance = require('chance');
 class FeedController {
 
   async index({ params,request, response }) {
@@ -11,9 +12,20 @@ class FeedController {
   }
 
   async store({ request }) {
-    const data = request.only(["comment", "title"]) 
-    const feed = await Feed.create(data)
-    return feed
+    var chance = new Chance();
+    var my_random_string = chance.string();
+
+  for (var i = 0; i < 10000; i++) {
+    const feed = await Feed.create({title:chance.name(),comment:chance.paragraph().substring(0,240)})
+    console.log('criado'+i)
+ }
+
+ return  'ok'
+
+
+    // const data = request.only(["comment", "title"]) 
+    // const feed = await Feed.create(data)
+    // return feed
   }
 
   async show({ params}) {
